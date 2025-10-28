a76e7f03bdd9b441f7c581c11774c226c0fb11dc
davidlin17 <davidkedua7@gmail.com>
2025-10-28 19:32:44 +0700

    Commit No. 2 – Add Google Maps API Key and pinpoint current user location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
